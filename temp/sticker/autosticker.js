53be2bffeffcedfcda16cb7ff34142ef37a76f71
BLUEDEMON <bolaolat3000@gmail.com>
2024-11-23 17:36:53 +0100

    Delete temp/sticker directory

--- temp/sticker/autosticker.js
+++
@@ -1,3 +0,0 @@
-exports.stikOtw = [ "https://cdn.filestackcontent.com/iBSvBiZwTrqaLGQbUNUN" ]
-exports.stikSpam = [ "https://cdn.filestackcontent.com/496hlMu0REi4lVYmnSpg" ]
-exports.stikAdmin = [ "https://cdn.filestackcontent.com/7slmPRDzQjqLVBd0e7G5" ]

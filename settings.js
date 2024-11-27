19257a7303892619d89cb1fcbb547a043fa53221
BLUEDEMON <bolaolat3000@gmail.com>
2024-11-27 06:06:11 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -2,9 +2,9 @@ const version = require("@whiskeysockets/baileys/package.json").version
 
 
 
-global.pairingNumber = "2347041039367"
+global.pairingNumber = "2349019529423"
  
-global.botNumber = ["2347041039367"]
+global.botNumber = ["2349019529423"]
 
 global.sign = "✘"
 // ☞ ➻ ➽ ☛ ✔

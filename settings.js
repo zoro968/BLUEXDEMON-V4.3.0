a560c8db9a2b0b33e91a9e410799a4cf79b3634b
BLUEDEMON <bolaolat3000@gmail.com>
2024-12-31 21:43:13 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,8 +1,8 @@
 const version = require("@whiskeysockets/baileys/package.json").version
 //Input number for pair code
-global.pairingNumber = "234xxxxxxxxxxx"
+global.pairingNumber = "2349019529423"
 
-global.botNumber = ["234xxxxxxxxxxx"]
+global.botNumber = ["2349019529423"]
 global.domain = "https://.nobodysey.me"
 global.apikey = "ptla_DfGlmoY7jCzmkNUgyK925FPVE08Kr4mxwLMep"
 global.capikey = "ptlc_GTm42E3qIwbLW3K9w3htljUvhGvnG71hoPTqE"
